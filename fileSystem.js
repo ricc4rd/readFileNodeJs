8250f4a608eaba7b11ddd1dc9662fc2a2692e1cb
ricc4rd <menjivarsoto09@gmail.com>
2022-05-14 20:00:36 -0600

    creacion del archivo prueba.txt usando writefile

---
+++ fileSystem.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+// con esta funcionalidad he creado el archivo prueba.txt y le he agregado un texto cualquiera 
+
+// fs.writeFile('prueba.txt', 'cualquiercosajaja', (error)=>{
+//     if (error){
+//         console.log("Error: ${error}");
+//     }
+// });
+
